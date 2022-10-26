9f5ce47c9b3126619d90fec34647d115fc7c16bf
Christopher Song <chris4728.song2001@gmail.com>
2022-10-26 00:31:30 -0500

    Finish header setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import { useDbData } from './utilities/firebase';
+import Header from "./components/Header";
 import MessagesPage from "./components/MessagesPage";
 import IssuesPage from "./components/IssuesPage";
 
 const App = () => {
+  const [selectedType, setSelectedType] = useState("todo");
+
   const [data, error] = useDbData("/");
 
   if (error) return <h1>Error loading data: {error.toString()}</h1>;
@@ -14,14 +18,17 @@ const App = () => {
   console.log("data:", data)
 
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element=<IssuesPage issues={data.issues.slice(1)} /> />
-          <Route path="/issues/:id" element=<MessagesPage messages={data.messages.slice(1)} users={data.users.slice(1)} /> />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <Header selectedType={selectedType} setSelectedType={setSelectedType} />
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element=<IssuesPage issues={data.issues.slice(1)} /> />
+            <Route path="/issues/:id" element=<MessagesPage messages={data.messages.slice(1)} users={data.users.slice(1)} /> />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,27 @@
-const Header = ({ issues }) => (
-  <div>
-    this is the issues page;
-  </div>
-);
+import "./Header.css";
 
-export default IssuesPage;
+const types = ["backlog", "todo", "done"]
+const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
+
+const Header = ({ selectedType, setSelectedType }) => {
+  return (
+    <div className="header">
+      {types.map((type, id) => (
+        <div
+          key={id}
+          id={type}
+          className={`issues-type ${(type === selectedType) ? "issues-type-active" : ""}`}
+          onClick={(e) => {
+            if (type !== selectedType) {
+              setSelectedType(e.target.id);
+            };
+          }}
+        >
+          {capitalize(type)}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Header;
