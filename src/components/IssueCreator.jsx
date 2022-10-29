2c7504fb5518bdb5319d2ea91e8a5e8791ea4de4
Adam Stewart <adamjoesph11@gmail.com>
2022-10-29 14:13:39 -0500

    fixing some jank css

--- src/components/IssueCreator.jsx
+++ src/components/IssueCreator.jsx
@@ -1,7 +1,7 @@
 import { useDbUpdate } from '../utilities/firebase'
 import { useState } from 'react';
 import './IssueCreator.css'
-const issueTypes = ["Todo","Done","Backlog"]
+const issueTypes = ["Todo", "Done", "Backlog"]
 
 const IssueCreator = ({ closeModal, change }) => {
     // need to generate an id (use hash function)
@@ -14,31 +14,33 @@ const IssueCreator = ({ closeModal, change }) => {
     const [issueType, setIssueType] = useState(issueTypes[0])
 
     const submit = (e) => {
-        if(e.target[0].value === ''){}
-        else{
-        e.preventDefault();
-        update({
-            contents: e.target[0].value,
-            issue_id: issueID,
-            status: e.target[1].value,
-        });
-        document.getElementById('issue-form').reset();
-    }
+        if (e.target[0].value === '') { }
+        else {
+            e.preventDefault();
+            update({
+                contents: e.target[0].value,
+                issue_id: issueID,
+                status: e.target[1].value,
+            });
+            document.getElementById('issue-form').reset();
+        }
         closeModal()
     };
 
     return (
-      <form id='issue-form' onSubmit={submit} className='field-container'>
-          <input className='issue-field' placeholder='Enter Task Name' onChange={change} />
-          <select className="select-bar" defaultValue={issueType} onChange={(e) => setIssueType(e.target.value)}>
-          {
-            issueTypes.map((issueType, id) => (
-              <option key={id} value={issueType}>{issueType}</option>
-            ))
-          }
-        </select>
-          <button className='issue-submit' type="submit">Submit</button>
-      </form>
+        <div>
+            <form id='issue-form' onSubmit={submit} className='issue-fields'>
+                <input className='issue-field' placeholder='Enter Task Name' onChange={change} />
+                <select className="select-bar" defaultValue={issueType} onChange={(e) => setIssueType(e.target.value)}>
+                    {
+                        issueTypes.map((issueType, id) => (
+                            <option key={id} value={issueType}>{issueType}</option>
+                        ))
+                    }
+                </select>
+            </form>
+            <button className='issue-submit' type="submit">Submit</button>
+        </div>
     );
 };
 
