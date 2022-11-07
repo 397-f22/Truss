787832208ac43abb9ec6b7d77c7fd7d444becee1
Christopher Song <chris4728.song2001@gmail.com>
2022-11-07 17:10:02 -0600

    Remove unnecessary error catching

--- src/components/IssuesPage.jsx
+++ src/components/IssuesPage.jsx
@@ -4,12 +4,8 @@ import { Navigate, useParams } from "react-router-dom";
 
 const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 
-const IssuesPage = ({ projectIDs, selectedType, issues }) => {
+const IssuesPage = ({ selectedType, issues }) => {
   const { project_id } = useParams();
-
-  if (!projectIDs.includes(parseInt(project_id))) {
-    return <Navigate to="/" />;
-  };
 
   const filteredIssues = issues.filter(issue => issue.status === capitalize(selectedType) && issue.project_id === parseInt(project_id));
 
--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -1,8 +1,8 @@
-import './IssueModal.css';
+import './ProjectModal.css';
 
 // https://codebuckets.com/2021/08/08/bootstrap-modal-dialog-in-react-without-jquery/
 
-const IssueModal = ({ children, open, close }) => {
+const ProjectModal = ({ children, open, close }) => {
     return (
       <div
         className={`modal ${open ? 'modal-show' : ''}`}
@@ -13,7 +13,7 @@ const IssueModal = ({ children, open, close }) => {
         <div className="modal-dialog" role="document">
           <div className="modal-content">
             <div className="modal-header">
-            <div className="issue-modal-header">Create a New Issue</div>
+            <div className="issue-modal-header">Create a New Project</div>
               <button type="button" className="btn-close" aria-label="Close" onClick={close} />
             </div>
             <div className="modal-body">
@@ -25,4 +25,4 @@ const IssueModal = ({ children, open, close }) => {
     );
   };
 
-export default IssueModal
+export default ProjectModal;
